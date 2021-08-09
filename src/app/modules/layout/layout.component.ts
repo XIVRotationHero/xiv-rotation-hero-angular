39bae604c8d2aab57310dad71f77d5d02d73e578
Daniel Klingmann <info@sargodarya.de>
2021-08-09 14:43:55 +0200

    feat: add layout dialog functionality

--- src/app/modules/layout/layout.component.ts
+++ src/app/modules/layout/layout.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {HotbarService} from "../hotbars/services/hotbar.service";
+import {FormArray, FormBuilder, FormGroup} from "@angular/forms";
+import {take} from "rxjs/operators";
+import {HotbarOptions} from "../hotbars/interfaces/hotbar-options";
 
 @Component({
   selector: 'rh-layout',
@@ -7,12 +10,44 @@ import {HotbarService} from "../hotbars/services/hotbar.service";
   styleUrls: ['./layout.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class LayoutComponent implements OnInit {
+export class LayoutComponent {
 
   public readonly hotbarSettings$ = this.hotbarService.hotbarSettings$;
 
-  constructor(private readonly hotbarService: HotbarService) { }
+  public form: FormGroup;
 
-  ngOnInit(): void {}
+  constructor(
+    private readonly hotbarService: HotbarService,
+    private readonly fb: FormBuilder
+  ) {
+    this.hotbarSettings$.pipe();
+
+    this.form = this.fb.group({
+      hotbarOptions: this.fb.array([])
+    });
+
+    this.hotbarSettings$.pipe(take(1))
+      .subscribe((hotbarOptions) => {
+        hotbarOptions.forEach((hotbarOption) => {
+          this.hotbarOptionsArray.push(this.createHotbarFormGroup(hotbarOption));
+        });
+      });
+
+    this.form.valueChanges.subscribe((value) => {
+      this.hotbarService.updateHotbarOptions(value.hotbarOptions);
+    });
+  }
+
+  public get hotbarOptionsArray(): FormArray {
+    return <FormArray>this.form.controls.hotbarOptions;
+  }
+
+  private createHotbarFormGroup(hotbarOption: HotbarOptions) {
+    return this.fb.group({
+      visible: this.fb.control(hotbarOption.visible),
+      hotbarStyle: this.fb.control(hotbarOption.hotbarStyle),
+      scale: this.fb.control(hotbarOption.scale)
+    });
+  }
 
 }
