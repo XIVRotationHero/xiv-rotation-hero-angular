3f1d771202c21095b3c74d2c8c850a5c95960690
Daniel Klingmann <info@sargodarya.de>
2021-08-17 12:10:48 +0200

    chore: properly implement local development behavior

--- src/environments/environment.local.ts
+++
@@ -1,7 +0,0 @@
-// Copy this file and name it environment.local.ts
-// Then run npm run start:local
-
-export const environment = {
-  production: false,
-  apiBaseUrl: 'https://api.xivrotationhero.com'
-};
