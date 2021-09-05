d28958e01bfb8a6d39cd6de955ea2afc7c14624b
Daniel Klingmann <info@sargodarya.de>
2021-09-05 14:04:36 +0200

    fix: add missing local-storage-persistance-key

---
+++ src/app/modules/configuration/enums/local-storage-persistance-key.ts
@@ -0,0 +1,6 @@
+export enum LocalStoragePersistanceKey {
+  InputType = 'rh-input-type',
+  HotbarDisplaySettings = 'rh-hotbar-display-settings',
+  HotbarCrossSettings = 'rh-hotbar-cross-settings',
+  HotbarCustomSettings = 'rh-hotbar-custom-settings'
+}
